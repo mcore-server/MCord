3e5278e4180fc32dcd69713da3a1050a28e27bb0
wr4ck3r <wr4ck3r@proton.me>
2022-07-18 17:55:53 +0300

    added loguru; more events for logs

--- cogs/discord_logging.py
+++ cogs/discord_logging.py
@@ -3,12 +3,13 @@ import sqlite3
 
 from discord.ext import commands
 
+
 class Logging:
 	def __init__(self, bot, con):
-		self.LOG_CHANNEL_1 = 992871458630013038 # Message logs channel
-		self.LOG_CHANNEL_2 = 992871624514740264 # Event logs channel
-		self.LOG_CHANNEL_3 = 992871638540505158 # Voice logs channel
-		self.LOG_CHANNEL_4 = 992871652918566992 # Bot logs channel
+		self.LOG_CHANNEL_1 = 992871458630013038  # Message logs channel
+		self.LOG_CHANNEL_2 = 992871624514740264  # Event logs channel
+		self.LOG_CHANNEL_3 = 992871638540505158  # Voice logs channel
+		self.LOG_CHANNEL_4 = 992871652918566992  # Bot logs channel
 		self.LOG_TYPES = {
 			1: self.LOG_CHANNEL_1,
 			2: self.LOG_CHANNEL_2,
@@ -18,21 +19,25 @@ class Logging:
 		self.bot = bot
 		self.con = con
 		self.cur = con.cursor()
+
 	async def write_log(self, log_type, log_content, attachment=None):
 		log_channel = self.bot.get_channel(self.LOG_TYPES[log_type])
 		all_logs = self.cur.execute('SELECT * FROM log').fetchall()
 
 		log_id = len(all_logs) + 1
 
-		embed = discord.Embed(title=f'#{log_id} [{log_type}]', description=log_content)
+		embed = discord.Embed(
+			title=f'#{log_id} [{log_type}]', description=log_content)
 
 		if attachment:
 			embed.set_image(attachment)
 
 		log_message = await log_channel.send(embed=embed)
 
-		self.cur.execute('INSERT INTO log VALUES (?, ?, ?, ?)', (log_id, log_type, log_message.id, log_content,))
+		self.cur.execute('INSERT INTO log VALUES (?, ?, ?, ?)',
+						 (log_id, log_type, log_message.id, log_content,))
 		self.con.commit()
+
 
 class LoggingCog(commands.Cog):
 	def __init__(self, bot):
@@ -47,82 +52,102 @@ class LoggingCog(commands.Cog):
 		)''')
 		self.con.commit()
 
-	@commands.Cog.listener()
-	async def on_message_delete(self, m):
-		created_at = m.created_at.strftime('%d-%m-%Y %H:%M:%S')
 
-		log_content = f'{m.author} удалил сообщение.\n[{created_at}] "{m.content}"\nКанал: <#{message.channel.id}>'
+@commands.Cog.listener()
+async def on_message(self, m):
+	msg = m.content.lower()
 
-		await Logging(self.bot, self.con).write_log(1, log_content)
+	if m.author == self.bot.user:
+		return
 
-	@commands.Cog.listener()
-	async def on_message_edit(self, before, after):
-		created_at = after.created_at.strftime('%d-%m-%Y %H:%M:%S')
-		edited_at = after.edited_at.strftime('%d-%m-%Y %H:%M:%S')
-
-		log_content = f'{message.author} отредактировал сообщение.\n[{created_at}] "{before.content}"\n[{edited_at}] "{after.content}"\nКанал: <#{after.channel.id}>'
-
-		await Logging(self.bot, self.con).write_log(1, log_content)
-
-	@commands.Cog.listener()
-	async def on_raw_reaction_add(self, payload):
-		channel = self.bot.get_channel(payload.channel_id)
-		message = await channel.fetch_message(payload.message_id)
-		created_at = message.created_at.strftime('%d-%m-%Y %H:%M:%S')
-
-		log_content = f'{payload.member} оставил реакцию {payload.emoji} под сообщением от {message.author}.\n[{created_at}] "{message.content}"\nКанал: <#{message.channel.id}>'
-
-		await Logging(self.bot, self.con).write_log(2, log_content)
-
-	@commands.Cog.listener()
-	async def on_raw_reaction_remove(self, payload):
-		channel = self.bot.get_channel(payload.channel_id)
-		message = await channel.fetch_message(payload.message_id)
-		created_at = message.created_at.strftime('%d-%m-%Y %H:%M:%S')
-
-		log_content = f'{payload.member} убрал реакцию {payload.emoji} под сообщением от {message.author}.\n[{created_at}] "{message.content}"\nКанал: <#{message.channel.id}>'
-
-		await Logging(self.bot, self.con).write_log(2, log_content)
-
-	@commands.Cog.listener()
-	async def on_raw_reaction_clear(self, payload):
-		channel = self.bot.get_channel(payload.channel_id)
-		message = await channel.fetch_message(payload.message_id)
-
-		log_content = f'Реакции были очищены под [сообщением](http://discord.com/channels/{channel.guild.id}/{channel.id}/{message.id}) в канале <#{channel.id}>'
-
-		await Logging(self.bot, self.con).write_log(2, log_content)
-
-	@commands.Cog.listener()
-	async def on_member_join(self, member):
-		log_content = f'Участник {member.mention} ({member}) присоединился к Discord серверу.'
-
-		await Logging(self.bot, self.con).write_log(2, log_content)
-
-	@commands.Cog.listener()
-	async def on_member_remove(self, member):
-		log_content = f'Участник {member.mention} ({member}) покинул Discord сервер.'
-
-		await Logging(self.bot, self.con).write_log(2, log_content)
-
-	@commands.Cog.listener()
-	async def on_member_ban(self, guild, user):
-		log_content = f'Участник {member.mention} ({member}) был заблокирован.'
-
-		await Logging(self.bot, self.con).write_log(2, log_content)
-
-	@commands.Cog.listener()
-	async def on_voice_state_update(self, member, before, after):
-		if before.channel != after.channel and after.channel is not None:
-			log_content = f'{member} присоединился к каналу {after.channel.name} [<#{after.channel.id}>].'
-
-			await Logging(self.bot, self.con).write_log(3, log_content)
+	# sudo commands
+	if msg.startswith('#'):
+		if not m.author.guild_permissions.mention_everyone:
 			return
-		for before_param in before:
-			print(before_param.index(before))
+		command = msg[1:]
+		match command:
+			case 'log':
+				arg = command.split(' ')[1:]
+				if len(arg) == 1:
+					db_log = self.cur.execute(
+						'SELECT * FROM logs WHERE id = ?', (int(arg)[0])).fetchone()
+					await m.channel.send()
+				else:
+					await m.channel.send('`#log <id>`')
+					return
 
-		#await Logging(self.bot, self.con).write_log(2, log_content)
+
+@commands.Cog.listener()
+async def on_message_delete(self, m):
+	created_at = m.created_at.strftime('%d-%m-%Y %H:%M:%S')
+
+	log_content = f'{m.author} удалил сообщение.\n[{created_at}] "{m.content}"\nКанал: <#{m.channel.id}>'
+
+	await Logging(self.bot, self.con).write_log(1, log_content)
+
+
+@commands.Cog.listener()
+async def on_message_edit(self, before, after):
+	created_at = after.created_at.strftime('%d-%m-%Y %H:%M:%S')
+	edited_at = after.edited_at.strftime('%d-%m-%Y %H:%M:%S')
+
+	log_content = f'{after.author} отредактировал сообщение.\n[{created_at}] "{before.content}"\n[{edited_at}] "{after.content}"\nКанал: <#{after.channel.id}>'
+
+	await Logging(self.bot, self.con).write_log(1, log_content)
+
+
+@commands.Cog.listener()
+async def on_raw_reaction_add(self, payload):
+	channel = self.bot.get_channel(payload.channel_id)
+	message = await channel.fetch_message(payload.message_id)
+	created_at = message.created_at.strftime('%d-%m-%Y %H:%M:%S')
+
+	log_content = f'{payload.member} оставил реакцию {payload.emoji} под сообщением от {message.author}.\n[{created_at}] "{message.content}"\nКанал: <#{message.channel.id}>'
+
+	await Logging(self.bot, self.con).write_log(2, log_content)
+
+
+@commands.Cog.listener()
+async def on_raw_reaction_remove(self, payload):
+	channel = self.bot.get_channel(payload.channel_id)
+	message = await channel.fetch_message(payload.message_id)
+	created_at = message.created_at.strftime('%d-%m-%Y %H:%M:%S')
+
+	log_content = f'{payload.member} убрал реакцию {payload.emoji} под сообщением от {message.author}.\n[{created_at}] "{message.content}"\nКанал: <#{message.channel.id}>'
+
+	await Logging(self.bot, self.con).write_log(2, log_content)
+
+
+@commands.Cog.listener()
+async def on_raw_reaction_clear(self, payload):
+	channel = self.bot.get_channel(payload.channel_id)
+	message = await channel.fetch_message(payload.message_id)
+
+	log_content = f'Реакции были очищены под [сообщением](http://discord.com/channels/{channel.guild.id}/{channel.id}/{message.id}) в канале <#{channel.id}>'
+
+	await Logging(self.bot, self.con).write_log(2, log_content)
+
+
+@commands.Cog.listener()
+async def on_member_join(self, member):
+	log_content = f'Участник {member.mention} ({member}) присоединился к Discord серверу.'
+
+	await Logging(self.bot, self.con).write_log(2, log_content)
+
+
+@commands.Cog.listener()
+async def on_member_remove(self, member):
+	log_content = f'Участник {member.mention} ({member}) покинул Discord сервер.'
+
+	await Logging(self.bot, self.con).write_log(2, log_content)
+
+
+@commands.Cog.listener()
+async def on_member_ban(self, _, member):
+	log_content = f'Участник {member.mention} ({member}) был заблокирован.'
+
+	await Logging(self.bot, self.con).write_log(2, log_content)
 
 
 def setup(bot):
-    bot.add_cog(LoggingCog(bot))
+	bot.add_cog(LoggingCog(bot))
--- main.py
+++ main.py
@@ -1,38 +1,48 @@
 #!/usr/bin/env python3.10
-import discord, requests, json
-import random, os, subprocess
-import asyncio, textwrap, logging
+import discord
+import requests
+import json
+import random
+import os
+import subprocess
+import asyncio
+import textwrap
 import sqlite3
+import sys
 
 from datetime import datetime
 from discord.ext import commands
 from discord.ui import Button, View
 from mcrcon import MCRcon
+from loguru import logger
+from shutil import copyfile
 
-bot = commands.Bot(command_prefix='!',intents=discord.Intents.all())
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 bot.remove_command('help')
 
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='[%H:%M:%S]', level=logging.INFO)
-
+logger.remove()
+logger.add(sys.stdout, format="[{time:HH:mm:ss}] <lvl>{message}</lvl>", level='INFO')
 con = sqlite3.connect('db/bot.db')
 cur = con.cursor()
 
 try:
-	cred_file = open('configuration.secret', 'r')
-	configuration = cred_file.read().split('\n')
-	cred_file.close()
-except FileNotFoundError:
-	logging.info('Creating configuration file for you...')
-	open('configuration.secret', 'w').write('discord bot token\nminecraft server ip\nminecraft rcon port\nminecraft rcon password\nfull path to your minecraft server')
-	logging.info('edit configuration.secret text file and run me!')
+	import config
+except ModuleNotFoundError:
+	logger.error('Your configuration file not found!')
+	copyfile('examples/config.py', 'config.py')
+	logger.info('Please edit config.py to run bot')
 	exit()
 
-BOT_TOKEN = configuration[0]
+if getattr(config, "token", None) == 'TOKEN':
+	logger.error('You did not edited the config file! Please edit it (config.py)')
+	exit()
 
-SERVER_IP = configuration[1]
-RCON_PORT = int(configuration[2])
-RCON_PASSWD = configuration[3]
-SERVER_PATH = configuration[4]
+BOT_TOKEN = getattr(config, "token", None)
+
+SERVER_IP = getattr(config, "ip", None)
+RCON_PORT = getattr(config, "rport", None)
+RCON_PASSWD = getattr(config, "rpasswd", None)
+SERVER_PATH = getattr(config, "server_path", None)
 
 if SERVER_PATH.endswith('/'):
 	SERVER_PATH = SERVER_PATH[:-1]
@@ -45,6 +55,7 @@ class MinecraftServer:
 		os.chdir(SERVER_PATH)
 		proc = subprocess.Popen(executable, stdout=subprocess.DEVNULL)
 		return proc
+
 	def console():
 		logs_file = open(SERVER_PATH + '/logs/latest.log')
 		logs = logs_file.read()
@@ -52,6 +63,8 @@ class MinecraftServer:
 		return logs
 
 # Long delay cycle
+
+
 async def long_delay():
 	paper_builds = await paper_update()
 	while True:
@@ -67,6 +80,7 @@ async def long_delay():
 		# Update statistics in voice channels
 		await update_stats()
 
+
 async def console_logger():
 	# Get server logs
 	console_log = MinecraftServer.console()
@@ -76,13 +90,15 @@ async def console_logger():
 
 		new_clog = MinecraftServer.console()
 
-		# some bugs, need to be fixed
-		if len(new_clog) != len(console_log) and len(new_clog) > 1:
-			if 'Thread RCON Client /127.0.0.1' in new_clog: return
+		print(new_clog)
+
+		if len(new_clog) != len(console_log):
 			console_log = new_clog.replace(console_log, '')
+			#if 'Thread RCON Client /127.0.0.1' in console_log:
+			#	return
 
 			if len(console_log) > 2000:
-				for part in textwrap.wrap(console_log, 2000):
+				for part in textwrap.wrap(console_log, 1999):
 					await console_channel.send(f'`{part}`')
 					return
 
@@ -91,12 +107,15 @@ async def console_logger():
 ###############################
 
 # Check Paper updates
+
+@logger.catch()
 async def paper_update(function='get', *args):
 	match function:
 		case 'get':
 			# get list of paper builds
-			paper_builds = json.loads(requests.get('https://api.papermc.io/v2/projects/paper/version_group/1.19/builds').text)["builds"]
-			return paper_builds
+			paper_builds = json.loads(requests.get(
+				'https://api.papermc.io/v2/projects/paper/version_group/1.19/builds').text)["builds"]
+			return paper_builds	
 		case 'check':
 			# check updates
 			if len(args[0]) < len(args[1]):
@@ -105,12 +124,17 @@ async def paper_update(function='get', *args):
 				paper_builds = args[1]
 
 				last_build = paper_builds[-1]
-		
+
 				# some variables
-				number = last_build["build"]; version = last_build["version"]; changes = last_build["changes"]; commit_url = f'https://github.com/paperMC/paper/commit/{last_build["changes"][0]["commit"]}'; download_url = f'https://api.papermc.io/v2/projects/paper/versions/{version}/builds/{number}/downloads/paper-{version}-{number}.jar'
+				number = last_build["build"]
+				version = last_build["version"]
+				changes = last_build["changes"]
+				commit_url = f'https://github.com/paperMC/paper/commit/{last_build["changes"][0]["commit"]}'
+				download_url = f'https://api.papermc.io/v2/projects/paper/versions/{version}/builds/{number}/downloads/paper-{version}-{number}.jar'
 
 				# generate buttons
-				buttons = [Button(label='Скачать', style=discord.ButtonStyle.green, url=download_url),Button(label='GitHub', style=discord.ButtonStyle.green, url=commit_url)]
+				buttons = [Button(label='Скачать', style=discord.ButtonStyle.green, url=download_url), Button(
+					label='GitHub', style=discord.ButtonStyle.green, url=commit_url)] # buttons download and github
 				view = View()
 
 				# append buttons to list
@@ -118,12 +142,14 @@ async def paper_update(function='get', *args):
 					view.add_item(btn)
 
 				# generate embed
-				embed=discord.Embed(title='Найдено обновление!', description=f'`PaperMC {version} | #{number}`\n`{changes[0]["summary"]}`')
-				embed.set_author(name='PaperMC', icon_url='https://media.discordapp.net/attachments/981094724939153448/994578831996366878/unknown.png')
+				embed = discord.Embed(
+					title='Найдено обновление!', description=f'`PaperMC {version} | #{number}`\n`{changes[0]["summary"]}`') # found update
+				embed.set_author(
+					name='PaperMC', icon_url='https://media.discordapp.net/attachments/981094724939153448/994578831996366878/unknown.png')
 				embed.timestamp = datetime.now()
 
 				await update_channel.send(embed=embed, view=view)
-				
+
 				# return true value because there's new paper version
 				return True
 			else:
@@ -131,9 +157,13 @@ async def paper_update(function='get', *args):
 				return False
 
 # Update statistics category
+
+
 async def update_stats():
-	online_channel = bot.get_channel(984165492224831508) # Channel that show server online
-	tps_channel = bot.get_channel(984165542699085855) # Channel that show server TPS
+	# Channel that show server online
+	online_channel = bot.get_channel(984165492224831508)
+	# Channel that show server TPS
+	tps_channel = bot.get_channel(984165542699085855)
 
 	try:
 		with MCRcon(SERVER_IP, RCON_PASSWD, port=RCON_PORT) as mcr:
@@ -150,47 +180,57 @@ async def update_stats():
 
 ###############################
 
+
 @bot.event
 async def on_ready():
 	#cur.execute('''create table''')
-	#con.commit()
+	# con.commit()
 	took_time = datetime.now()-START_TIME
-	logging.info(f'Done! (took {took_time.seconds}s)')
+	logger.info(f'Done! (took {took_time.seconds}s)')
 	bot.loop.create_task(long_delay())
+
 
 @bot.event
 async def on_message(m):
-	# await bot.process_commands(m)
-	#global proc
 	msg = m.content.lower()
-	print()
 
-	if m.author == bot.user: return
+	if m.author == bot.user:
+		return
 
 	if m.channel.id == 984545711364386856:
 		match msg:
 			case 'start':
-				logging.warning(f'{m.author.name}#{m.author.discriminator} started server')
-				await m.channel.send(':white_check_mark: `Запускаю сервер...`')
+				logger.warning(
+					f'{m.author.name}#{m.author.discriminator} started server')
+				await m.channel.send(':white_check_mark: `Запускаю сервер...`')  # ':white_check_mark: `Starting server...`'
 				MinecraftServer.start()
 				bot.loop.create_task(console_logger())
 			case _:
+				if msg.startswith('#'):
+					return
 				try:
 					with MCRcon(SERVER_IP, RCON_PASSWD, port=RCON_PORT) as mcr:
 						output = mcr.command(m.content)
-					logging.warning(f'{m.author.name}#{m.author.discriminator} sended command: {m.content}')
+					logger.warning(
+						f'{m.author.name}#{m.author.discriminator} issued RCON command: {m.content}')
 					await m.channel.send(f'`{output}`')
 				except ConnectionRefusedError:
-					await m.channel.send('Сервер не запущен. Напишите `start` для запуска.')
+					await m.channel.send('Сервер не запущен. Напишите `start` для запуска.')  # 'Server not started. Type `start` for run server.'
 		return
-	
+
 	# sudo commands
 	if msg.startswith('#'):
-		if not m.author.guild_permissions.mention_everyone: return
+		if not m.author.guild_permissions.mention_everyone:
+			return
 		command = msg[1:]
 		match command:
 			case 'log':
-				pass
+				arg = command.split(' ')[1:]
+				if len(arg) == 1:
+					db_log = cur.execute('SELECT * FROM logs WHERE id = ?', (arg[0])).fetchone()
+				else:
+					await m.channel.send('`#log <id>`')
+					return
 
 	match msg:
 		case '!ping':
@@ -198,11 +238,12 @@ async def on_message(m):
 
 
 @bot.event
-async def on_voice_state_update(member,_,after):
+async def on_voice_state_update(member, _, after):
 	if after.channel is not None:
 		if after.channel.id == 981955747577475083:
 			# get category; define variables
-			ctg = discord.utils.get(member.guild.categories, id=961942312588542022)
+			ctg = discord.utils.get(
+				member.guild.categories, id=961942312588542022)
 			emojis = ['🔥', '⭐️', '⚡️', '✨']
 			username = member.name
 
@@ -210,7 +251,7 @@ async def on_voice_state_update(member,_,after):
 				username = member.nick
 
 			# create voice channel, add permissions and move member to this channel
-			chn = await member.guild.create_voice_channel(name=f"{random.choice(emojis)} {username}",category=ctg)
+			chn = await member.guild.create_voice_channel(name=f"{random.choice(emojis)} {username}", category=ctg)
 
 			await chn.set_permissions(member, connect=True, move_members=True, manage_channels=True)
 			await member.move_to(chn)
@@ -219,13 +260,13 @@ async def on_voice_state_update(member,_,after):
 				# is channel empty?
 				return len(chn.members) == 0
 
-			await bot.wait_for('voice_state_update',check=check)
+			await bot.wait_for('voice_state_update', check=check)
 			await chn.delete()
 
 if __name__ == '__main__':
-	logging.info('Starting...')
-	for file in os.listdir("cogs"): 
-		if file.endswith(".py"): 
+	logger.info('Starting...')
+	for file in os.listdir("cogs"):
+		if file.endswith(".py"):
 			name = file[:-3]
 			bot.load_extension(f"cogs.{name}")
 	bot.run(BOT_TOKEN)
